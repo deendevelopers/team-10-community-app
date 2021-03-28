fbf055dd0e0b7b27a1c499203916c4910f0b1cf7
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-03-28 12:44:22 +0100

    Updated

--- src/features/ui/CommunityCard.jsx
+++ src/features/ui/CommunityCard.jsx
@@ -2,36 +2,39 @@ import React from 'react'
 
 const CommunityCard = () => {
     return (
-        <div>
-            <div class="bg-white w-full p-2 rounded flex justify-center font-sans">
-<div class="rounded bg-grey-light w-64 p-2">
-  <div class="bg-white p-2 rounded mt-1">
-  	<h3 class="bg-white p-2 rounded mt-1  border-b border-black">Longsight Community</h3>
-  </div>
-  <div class="text-sm mt-2">
-  	<div class="bg-white p-2 rounded mt-1">
-        Do a mobile first layout
-	</div>
+        <div class="flex items-center justify-center min-h-screen ">
+        <div class="flex flex-col items-center w-full max-w-xs p-4 bg-white rounded-3xl md:flex-row">
+            <div >
     
-  	<div class="bg-white p-2 rounded mt-1 ">
-        Check the meta tags
-	</div>
-    
-  	<div class="bg-white p-2 rounded mt-1 cursor-pointer ">
-        Check the responsive layout on all devices
-      	<div class="text-grey-darker mt-2 ml-2 flex justify-between items-start">
-          	<span class="text-xs flex items-center">
-      			<svg class="h-4 fill-current mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path d="M11 4c-3.855 0-7 3.145-7 7v28c0 3.855 3.145 7 7 7h28c3.855 0 7-3.145 7-7V11c0-3.855-3.145-7-7-7zm0 2h28c2.773 0 5 2.227 5 5v28c0 2.773-2.227 5-5 5H11c-2.773 0-5-2.227-5-5V11c0-2.773 2.227-5 5-5zm25.234 9.832l-13.32 15.723-8.133-7.586-1.363 1.465 9.664 9.015 14.684-17.324z"/></svg>
-              	3/5
-          	</span>
-          	<img src="https://i.imgur.com/OZaT7jl.png" class="rounded-full" />
-      	</div>
-	</div>
-    <p class="mt-3 text-grey-dark">Add a card...</p>
-  </div>
-</div>
-</div>
+            </div>
+            <div class="flex flex-col space-y-4 ">
+            <div class="flex flex-col items-center md:items-start">
+         
+        <img
+            class="w-auto h-48"
+            src="https://i.imgur.com/kbUhOko.png"
+            alt="Ahmed Kamel"
+        />
+      
+                <h1 class="text-xl font-medium ">Longsight Community</h1>
+                
+                <h2 class="text-base font-medium text-gray-400">Activity</h2>
+           
+                <p class="text-sm">Homeless Shelter - Language Support - Helping Hand - FoodBank </p>
+
+                
+                <h2 class="text-base font-medium text-gray-400">Address</h2>
+                <p class="text-sm"> Longsight Community Church of the Nazarene, Tollgate Close, Manchester</p>
+            </div>
+            <div class="flex items-center justify-center space-x-3 md:justify-start">
+               
+            </div>
+            </div>
         </div>
+    
+         
+    
+    </div>
     )
 }
 
